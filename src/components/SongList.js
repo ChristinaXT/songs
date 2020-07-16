770562c4c10ef46c589c55fcb43daea375ed18da
Christina Williams <christinaxtwilliams@gmail.com>
2020-07-16 15:25:07 -0400

    added renderList

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class SongList extends Component {
+  renderList() {
+    return this.props.songs.map(() => {
+
+    });
+  }
+
   render() {
     return <div>SongList</div>;
   }
