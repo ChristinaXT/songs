644d58b2c41a23e380da543945c18eebebeddb19
Christina Williams <christinaxtwilliams@gmail.com>
2020-07-20 14:14:45 -0400

    added onclick

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -4,11 +4,14 @@ import { selectSong } from '../actions';
 
 class SongList extends Component {
   renderList() {
-    return this.props.songs.map((song) => {
+    return this.props.songs.map(song => {
        return (
          <div className="item" key={song.title}>
            <div className="right floated content">
-             <button className="ui button primary">
+             <button
+                className="ui button primary"
+                 onClick={() => this.props.selectSong(song)}
+                >
                Select
                </button>
            </div>
